02ec34a16eb4b6441f4d45495a6279b4c58763a3
David Bazan <davidbazan@ucsb.edu>
2025-10-08 17:05:25 -0700

    DB - created equals test and hashcode test

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -8,10 +8,22 @@ import org.junit.jupiter.api.Test;
 public class TeamTest {
 
     Team team;
+    Team team2;
+    Team team3;
+    Team team4;
+    Team t;
+
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");
+        team2 = new Team("test-team");
+        team3 = new Team("test-team");
+        team4 = new Team("test-team");
+        team2.addMember("Bob");
+        team3.addMember("Bob");
+        team4.addMember("Carl");
+        t = new Team("test-team");
     }
 
     @Test
@@ -19,8 +31,27 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
+   @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void equals_test(){
+        
+        
+        assertEquals(true,team.equals(team));
+        assertEquals(false,team.equals(15));
+        assertEquals(true,team2.equals(team3));
+        assertEquals(false,team2.equals(team4));
 
+    }
+    
+    @Test
+    public void hashcode_test(){
+        int result = t.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
 }
